dae297bf2ffc0c4a060aa64aae030619dc0eda87
Facundo Allemand <facundo.allemand@mercadolibre.com>
2018-10-05 23:29:18 -0300

    add basic SearchPage

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import {
   HashRouter as Router, Route, Switch, Redirect,
 } from 'react-router-dom';
 import { render } from 'react-dom';
+import SearchPage from './pages/search/SearchPage';
 
 import './assets/images/logo.svg';
 import './styles/_common.scss';
@@ -14,6 +15,7 @@ render((
   <Router>
     <Switch>
       <Route exact path="/" render={() => <div>Initial Page</div>} />
+      <Route exact path="/search" component={SearchPage} />
     </Switch>
   </Router>
 ), document.getElementById('root-app'));
---
+++ app/pages/search/SearchPage.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import queryString from 'query-string';
+import Filter from '../../components/Filter';
+import GameCard from '../../components/GameCard';
+import Pagination from '../../components/Pagination';
+import gamesService from '../../services/games.service';
+import './search.scss';
+
+class SearchPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.games = [];
+  }
+
+  componentWillMount() {
+    this.games = gamesService.getGamesWithImages();
+  }
+
+  render() {
+    const { games } = this;
+    const { history } = this.props;
+    return (
+      <div className="games">
+        <div className="games__actions">
+          Actions!
+        </div>
+        <div className={classnames(
+          'games__list',
+          { 'games__list--loading': false },
+        )}
+        >
+          {games.map(game => <GameCard game={game} />)}
+        </div>
+        <Pagination
+          active={1}
+          total={games.length}
+          show={10}
+          onChange={() => {}}
+        />
+      </div>
+    );
+  }
+}
+
+SearchPage.propTypes = {
+  history: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
+};
+
+export default withRouter(SearchPage);
