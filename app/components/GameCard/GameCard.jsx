447425667c84d65c89f545b5d00b793ed7a77b2a
Facundo Allemand <facundo.allemand@mercadolibre.com>
2018-10-05 23:27:56 -0300

    add GameCard component

---
+++ app/components/GameCard/GameCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import './_game-card.scss';
+
+const GameCard = ({ className, game }) => (
+  <div className={classnames(className, 'game-card')}>
+    <img className="game-card__image" src={game.images.banner} alt={`${game.name}`} />
+    <div className="game-card__description">
+      <h3 className="game-card__title">{game.name}</h3>
+    </div>
+  </div>
+);
+
+GameCard.propTypes = {
+  className: PropTypes.string,
+  game: PropTypes.object.isRequired,
+};
+
+GameCard.defaultProps = {
+  className: null,
+};
+
+export default GameCard;
